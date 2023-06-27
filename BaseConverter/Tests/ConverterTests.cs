c7a08c0c580bc74f09653fda833f715551221c3a
Gwenyfar <iruoma.onyia@cyphercrescent.com>
2023-06-27 16:15:26 +0100

    include unit tests

---
+++ BaseConverter/Tests/ConverterTests.cs
@@ -0,0 +1,25 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BaseConverter.Tests
+{
+    [TestFixture]
+    public class ConverterTests
+    {
+        [Test]
+        public void Should_Convert_From_a_Base_to_Another()
+        {
+            //arrange
+            var base17Number = 10;
+            //act
+            var base2Number = Converter.Convert(base17Number, 2, 17);
+
+            //assert
+            Assert.That(base2Number, Is.EqualTo("10001"));
+        }
+    }
+}
